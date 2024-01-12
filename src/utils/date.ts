bab2d7a42688c3669e17e41d8724ee6d3103666f
Thada Wangthammang <thada.wth@gmail.com>
2024-01-12 22:45:03 +0700

    init project

---
+++ src/utils/date.ts
@@ -0,0 +1,7 @@
+import dayjs from "dayjs";
+
+const dateFormats = 'YYYY-MM-DD HH:mm:ss';
+
+export function formatDate(date: Date) {
+  return dayjs(date).format(dateFormats);
+}
---
+++ src/view-server/server.ts
@@ -0,0 +1,44 @@
+import express from 'express';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
+import { formatDate } from '../utils/date';
+export const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+export async function startViewServer<T extends Record<string, unknown> = any>(data: Record<string, unknown>[]) {
+  const app = express();
+  const server = createServer(app);
+  const io = new Server(server);
+  const port = 3001;
+
+  let isClientConnected = false;
+
+  io.on('connection', socket => {
+    console.log('User connected');
+    isClientConnected = true;
+  });
+
+  // Set the custom path for EJS views
+  app.set('views', __dirname + '/views');
+
+  // Set the view engine to EJS
+  app.set('view engine', 'ejs');
+
+  // Middleware to add the formatDate function to locals
+  app.use((req, res, next) => {
+    res.locals.formatDate = formatDate;
+    next();
+  });
+
+  // Define a route to render the HTML page
+  app.get('/', (req, res) => {
+    res.render('index', { tableData: data });
+  });
+
+  server.listen(port, async () => {
+    console.log(`Server is running at http://localhost:${port}`);
+    while (!isClientConnected) {
+      await delay(200);
+    }
+    io.emit('reload');
+  });
+}
