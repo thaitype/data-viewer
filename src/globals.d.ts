3a080d49333c6b01c8e7b3e7e3e51b8789d14540
Thada Wangthammang <thada.wth@gmail.com>
2024-01-17 19:27:11 +0700

    save

--- src/globals.d.ts
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/consistent-type-imports
-import * as _ from './main';
-
-declare global {
-  type DataViewer = _.DataViewer;
-  const $dataViewer: typeof _.dataViewer;
-}
--- src/globals.ts
+++ src/globals.ts
@@ -5,3 +5,8 @@
 import * as _ from './main.js';
 // eslint-disable-next-line no-undef
 Object.assign(global, _);
+
+declare global {
+  type DataViewer = _.DataViewer;
+  const $dataViewer: typeof _.dataViewer;
+}
