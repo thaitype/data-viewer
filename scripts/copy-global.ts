0519493e3045240e6bbc1bb26e6e4775a1b5bae8
Thada Wangthammang <thada.wth@gmail.com>
2024-01-17 19:08:32 +0700

    use js

--- scripts/copy-global.ts
+++ scripts/copy-global.ts
@@ -3,7 +3,7 @@ import fs from 'fs-extra';
 import path from 'node:path';
 
 async function main(){
-    await fs.copy(path.resolve(__dirname, '../src/globals.mjs'), path.resolve(__dirname, '../dist/globals.mjs'));
+    await fs.copy(path.resolve(__dirname, '../src/globals.js'), path.resolve(__dirname, '../dist/globals.js'));
     await fs.copy(path.resolve(__dirname, '../src/globals.d.ts'), path.resolve(__dirname, '../dist/globals.d.ts'));
 }
 
